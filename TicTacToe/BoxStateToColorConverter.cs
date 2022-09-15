30a1e296771c947cc870b4bb7a0429979477f05e
SpaceBeeGaming <space.bee.gaming@gmail.com>
2022-09-15 16:15:20 +0300

    Moved BoxStateColorConverter out from the LED folder, as it isn't LED specific code.

--- TicTacToe/BoxStateToColorConverter.cs
+++ TicTacToe/BoxStateToColorConverter.cs
@@ -2,20 +2,19 @@
 
 using TicTacToe.Board;
 
-namespace TicTacToe.Led;
+namespace TicTacToe;
 
 public static class BoxStateToColorConverter
 {
     private static readonly Color _oColor = Color.Red;
     private static readonly Color _xColor = Color.Blue;
     private static readonly Color _emptyColor = Color.Black;
-
-    public static Color GetColorForBox(Box box) =>
-        box.Player switch
-        {
-            Players.X => _xColor,
-            Players.O => _oColor,
+    public static Color GetColorForBox(this Box box) =>
+    box.Player switch
+    {
+        Players.X => _xColor,
+        Players.O => _oColor,
         Players.Null => _emptyColor,
         _ => throw new InvalidOperationException($"{box.Player} is not valid.")
-        };
+    };
 }
